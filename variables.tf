3e064da259de3874d451056556d74301ddacba40
jimb0 <jimccann@redhat.com>
2023-05-03 15:26:18 -0400

    test

--- variables.tf
+++ variables.tf
@@ -16,6 +16,13 @@ variable "enviro" {
 }
 
 
+# example enivro vars
+#  enviro = {
+#    "dev-sts-uvm-1" = { tg_remote_network_id = "UNg==", tg_subnet = "subnet-074", tg_security_group = ["sg-07f"], aws_key = "development-sts" }
+#    "dev-sts-uvm-2" = { tg_remote_network_id = "UNg==", tg_subnet = "subnet-074", tg_security_group = ["sg-07f"], aws_key = "development-sts" }
+#    "dev-lts-uvm-1" = { tg_remote_network_id = "UMg==", tg_subnet = "subnet-039", tg_security_group = ["sg-0da"], aws_key = "development-lts" }
+#    "dev-lts-uvm-2" = { tg_remote_network_id = "UMg==", tg_subnet = "subnet-039", tg_security_group = ["sg-0da"], aws_key = "development-lts" }
+#  }
 
 
 
